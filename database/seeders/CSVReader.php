143f1ec04c9a38f50c414097c6a58334fab09c27
Ervinne Sodusta <ervinnesodusta@gmail.com>
2023-06-18 16:38:05 +0800

    Quickly refactor CSV Reader to have buffer capability

--- database/seeders/CSVReader.php
+++ database/seeders/CSVReader.php
@@ -2,9 +2,16 @@
 
 namespace Database\Seeders;
 
+/**
+ * TODO: Technical Debt
+ * 
+ * Note that readRows is a technical debt, it's a modded version from betterprogramming
+ * that allows for buffered reads. Ideally the two should be separate but let's work with
+ * this for now as we have limited time.
+ */
 class CSVReader
 {
-    public function readRows($filename, $delimiter = ',')
+    public function readRows($filename, $delimiter = ',', $buffer = null, $bufferSize = null)
     {
         // Reference: https://betterprogramming.pub/how-to-seed-database-from-a-csv-file-in-laravel-83a54ce1015f
         // I didn't follow it completely as I hate putting CSVs on public folder.
@@ -21,18 +28,43 @@ class CSVReader
             return false;
         $header = null;
         $data = array();
+
+        $previousBufferPointer = $buffer ? $buffer['pointer'] : 0;
+
+        if (!$buffer) {
+            $buffer = [
+                'content' => [],
+                'pointer' => 0
+            ];
+        }   
+
         if (($handle = fopen($filename, 'r')) !== false) {
             while (($row = fgetcsv($handle, 1000, $delimiter)) !== false) {
-                // Note `preg_replace` below. Excel sometimes adds in an invisible 
-                // character that messes up our keys: 
-                // https://stackoverflow.com/questions/43414804/first-key-of-php-associative-array-returns-undefined-index-when-parsed-from-csv
-                if (!$header)
+                if (!$header) {
+                    // Note `preg_replace` below. Excel sometimes adds in an invisible 
+                    // character that messes up our keys: 
+                    // https://stackoverflow.com/questions/43414804/first-key-of-php-associative-array-returns-undefined-index-when-parsed-from-csv
                     $header = preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $row);
-                else
-                    $data[] = array_combine($header, $row);
+                    $buffer['pointer']++;
+                } else if ($previousBufferPointer < $buffer['pointer']) {
+                    // Not the best solution right now, but let's avoid overloading the
+                    // machine's ram by skipping contents based on the pointer.
+                    $buffer['content'][] = array_combine($header, $row);
+                    $buffer['pointer']++;
+                }
+
+                $reachedBufferLimit = ($previousBufferPointer + $bufferSize) < $buffer['pointer'];
+                if ($bufferSize > 0 && $reachedBufferLimit) {
+                    break;
+                }
             }
             fclose($handle);
         }
-        return $data;
-    }    
-}
+
+        if ($bufferSize == null) {
+            return $buffer['content'];
+        }
+
+        return $buffer;
+    }
+  }
